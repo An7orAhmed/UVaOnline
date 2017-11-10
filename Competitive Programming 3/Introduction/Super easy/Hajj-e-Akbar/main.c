58e8725b71404b0682b4e5bc53475f2d9095ed20
DESKTOP-UNQOVPE\antor <antor.mee@gmail.com>
2017-11-10 21:13:23 +0600

    no message

--- Competitive Programming 3/Introduction/Super easy/Hajj-e-Akbar/main.c
+++ Competitive Programming 3/Introduction/Super easy/Hajj-e-Akbar/main.c
@@ -3,6 +3,14 @@
 #include <string.h>
 
 int main(){
-
+    char txt[20];
+    int x = 1;
+    while(1){
+        scanf("%s", txt);
+        if(txt[0] == '*') exit(0);
+        if(strcmp(txt, "Hajj") == 0) printf("Case %d: Hajj-e-Akbar\n", x);
+        else printf("Case %d: Hajj-e-Asghar\n", x);
+        x++;
+    }
     return 0;
 }
