66a63cd3d043ae691e904ceacb42f212091d3b1b
AntorOfficial <antor.mee@gmail.com>
2019-08-25 04:00:23 +0600

    changed

---
+++ README.md
@@ -0,0 +1,5 @@
+# UVa username: AntorOfficial
+
+Online Judge ID:	<b>899243</b><br>
+Programming Language: <b>Python</b><br>
+Solving Start from: <b>12-Jul-2019</b>
---
+++ Volume 001/101 - The Blocks Problem/101 - The Blocks Problem.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ifstream cin("input.txt");
+    ofstream cout("output.txt");
+
+
+    return 0;
+}
